d172dd857151ac325ae4b8fb2eed7d37f6c10018
Nicolas <nicolasgfe3@gmail.com>
2023-06-12 21:45:23 -0300

    firt commit

--- App.tsx
+++ App.tsx
@@ -1,118 +1,50 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+import Geolocation from "@react-native-community/geolocation";
+import { useEffect, useState } from "react";
+import { View, Text, ActivityIndicator } from "react-native"
+import MapView, { Marker, enableLatestRenderer } from "react-native-maps"
 
-import React from 'react';
-import type {PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+const App = () => {
+  const [latitude, setLatitude] = useState(0);
+  const [longitude, setLongitude] = useState(0);
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+  enableLatestRenderer();
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
+  useEffect(() => {
+    Geolocation.getCurrentPosition(responde => {
+      setLatitude(responde.coords.latitude);
+      setLongitude(responde.coords.longitude);
+    }, erro => {
+      console.error(`Não foi possivel obter localização: ${erro.message}`)
+    }, {
+      enableHighAccuracy: true,
+      maximumAge: 0,
+    })
+  }, [])
 
-function Section({children, title}: SectionProps): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+  if(latitude === 0 ) {
+    return <ActivityIndicator style={{flex:1}} />;
+  }
+
+
   return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
+    <View style={{flex: 1}}>
+      <Text>Localização atual</Text>
+      <Text>{latitude}, {longitude}</Text>
+{/* Caso tenha a Chave-Key da api do google maps, precisa ser adicionado no android\app\src\main\AndroidManifest.xml
+      <MapView
+        style={{flex: 1}}
+        provider="google"
+        initialRegion={{
+          latitude,
+          longitude,
+          latitudeDelta: 1.5,
+          longitudeDelta: 1.5
+        }}
+        >
+          <Marker coordinate={{latitude, longitude}}/> 
+
+        </MapView> */}
     </View>
-  );
+  )
 }
-
-function App(): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-}
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
-
-export default App;
+export default App; 
